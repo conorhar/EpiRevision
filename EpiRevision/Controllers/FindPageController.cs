ee19d18b04e8261b0f798681f79b4b96dd0f8ef8
conorhar <conorhar.91@gmail.com>
2021-10-23 12:34:43 +0200

    Little more find.

--- EpiRevision/Controllers/FindPageController.cs
+++ EpiRevision/Controllers/FindPageController.cs
@@ -3,6 +3,11 @@ using EpiRevision.Models.ViewModels;
 using System.Web.Mvc;
 using EPiServer.Find;
 using EPiServer.Find.Framework;
+using EPiServer.Find.UnifiedSearch;
+using EPiServer.ServiceLocation;
+using EPiServer.Web.Routing;
+using EPiServer;
+using EPiServer.Core;
 
 namespace EpiRevision.Controllers
 {
@@ -11,6 +16,13 @@ namespace EpiRevision.Controllers
         public ActionResult Index(FindPage currentPage, string query)
         {
             var model = new ResultViewModel(currentPage);
+            var urlResolver = ServiceLocator.Current.GetInstance<UrlResolver>();
+            var contentLoader = ServiceLocator.Current.GetInstance<IContentLoader>();
+
+            var hitSpec = new HitSpecification
+            {
+                ExcerptLength = 255
+            };
 
             if (string.IsNullOrWhiteSpace(query))
             {
@@ -18,12 +30,34 @@ namespace EpiRevision.Controllers
             }
             else
             {
-                var result = SearchClient.Instance.UnifiedSearchFor(query, Language.English).GetResult();
+                var currentLanguage = GetCurrentLanguage(currentPage.Language.ToString());
+
+                var result = SearchClient.Instance.UnifiedSearchFor(query, currentLanguage)
+                    .Filter(x => !x.MatchType(typeof(FindPage))).GetResult(hitSpec);
+
+                //Casting result to page object to access properties
+                //foreach (var item in result.Hits)
+                //{
+                //    var content = urlResolver.Route(new UrlBuilder(item.Document.Url));
+                //    var page = contentLoader.Get<PageData>(content.ContentLink);
+                //    var startPage = contentLoader.Get<EpiStartPage>(content.ContentLink);
+                //}
 
                 model.Result = result;
             }
 
             return View(model);
+        }
+
+        private Language GetCurrentLanguage(string lang)
+        {
+            // Weird issue , en-gb does not return results properly unless set to Language.None
+            if (lang == "en-gb")
+            {
+                return Language.None;
+            }
+
+            return SearchClient.Instance.Settings.Languages.GetSupportedLanguage(lang) ?? Language.None;
         }
     }
 }
